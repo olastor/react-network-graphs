4815e2a3d5a977b5b1703a7adb66cfd66b4f6996
olastor <>
2018-11-18 09:40:36 +0100

    feat: Update style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
 import React, { Component } from 'react';
 import './App.css';
 import CytoscapeComponent from 'react-cytoscapejs';
-import { runInThisContext } from 'vm';
+import { Button } from '@material-ui/core';
+
+const stylesheet = [
+  {
+    selector: 'edge',
+    style: {
+      'label': 'data(label)',
+      'target-arrow-shape': 'triangle',
+      'target-arrow-color': '#ccc',
+      'line-color': '#ccc',
+      'target-endpoint': 'outside-to-line',
+      'curve-style': 'bezier'
+    }
+  },
+  {
+    selector: 'node',
+    style: {
+      'label': 'data(label)',
+      'text-valign': 'center',
+      'text-halign': 'center'
+    }
+  },
+  {
+    selector: '.labeled',
+    style: {
+      'border-width': 3,
+      'border-color': 'green'
+    }
+  },
+  {
+    selector: '.scanned',
+    style: {
+      'background-color': 'blue'
+    }
+  },
+  {
+    selector: '.marked-edge',
+    style: {
+      'line-color': 'black',
+      'target-arrow-color': 'black',
+    }
+  }
+];
 
 class App extends Component {
 
@@ -34,18 +76,21 @@ class App extends Component {
         3: {x : 200, y: 0},
         4: {x : 200, y: 100},
         5: {x : 300, y: 50}
-      }
+      },
+      stepCounter: 0,
+      terminated: false,
+      previousStates: []
     };
   }
 
   /**
    * Creates data structure for drawing network with cytoscape.
    */
-  toCytoscapeNetwork (state) {
+  toCytoscapeNetwork () {
     return [
       // nodes
       ...new Array(this.state.network.numberOfNodes).fill(undefined).map((_, i) => ({
-        data: { id: i, label: i },
+        data: { id: i, label: i === 0 ? 's' : (i === this.state.network.numberOfNodes - 1 ? 't' : i) },
         position: this.state.nodePositions[i],
         classes: `${this.state.nodesLabeled.includes(i) ? 'labeled ' : ''}${this.state.nodesScanned.includes(i) ? 'scanned ' : ''}`
       })),
@@ -65,11 +110,42 @@ class App extends Component {
    * Creates data structure for drawing residual network with cytoscape.
    */
   toCytoscapeResidual () {
+    return [
+      // nodes
+      ...new Array(this.state.network.numberOfNodes).fill(undefined).map((_, i) => ({
+        data: { id: i, label: i === 0 ? 's' : (i === this.state.network.numberOfNodes - 1 ? 't' : i) },
+        position: this.state.nodePositions[i],
+        classes: `${this.state.nodesLabeled.includes(i) ? 'labeled ' : ''}${this.state.nodesScanned.includes(i) ? 'scanned ' : ''}`
+      })),
+      // edges
+      ...this.state.network.edges.map(([nodeFrom, nodeTo, flow, capacity]) => ({
+        data: {
+          source: nodeFrom,
+          target: nodeTo,
+          label: `${capacity - flow}`
+        },
+        classes: this.state.nodesPre[nodeTo] === nodeFrom ? 'marked-edge' : ''
+      })),
+      ...this.state.network.edges.map(([nodeFrom, nodeTo, flow, capacity]) => ({
+        data: {
+          source: nodeTo,
+          target: nodeFrom,
+          label: `${flow}`
+        }
+      }))
+    ];
+  }
 
+  handlePrevStep() {
+    this.setState(state => state.previousStates[0])
   }
 
   handleNextStep() {
+    if (this.state.terminated) return;
+
     const t = this.state.network.numberOfNodes - 1;
+
+    this.setState(state => ({ previousStates: [state, ...state.previousStates] }));
 
     if (this.state.nodesLabeled.includes(t)) {
       // augment
@@ -146,7 +222,7 @@ class App extends Component {
       const i = this.state.nodesLabeled.filter(x => !this.state.nodesScanned.includes(x))[0];
 
       if (i === undefined) {
-        console.log('DONE');
+        this.setState({ terminated: true });
         return;
       }
 
@@ -173,61 +249,36 @@ class App extends Component {
       }), state => console.log(this.state));
     }
 
-    console.log(this.state)
+    this.setState(state => ({ stepCounter: state.stepCounter + 1 }));
   }
 
+  convertSetOfNodesToLabels (nodes) {
+    return nodes.map(x => x.toString().replace('0', 's').replace((this.state.network.numberOfNodes - 1).toString(), 't'));
+  }
 
   render () {
     const layout = { name: 'preset' };
     return (
       <div>
-        <button onClick={() => this.handleNextStep()}>Next</button>
-        <CytoscapeComponent
-          elements={this.toCytoscapeNetwork(this.state)}
-          layout={layout}
-          stylesheet={[
-            {
-              selector: 'edge',
-              style: {
-                'label': 'data(label)',
-                'target-arrow-shape': 'triangle',
-                'target-arrow-color': '#ccc',
-                'line-color': '#ccc',
-                'target-endpoint': 'outside-to-line',
-                'curve-style': 'bezier'
-              }
-            },
-            {
-              selector: 'node',
-              style: {
-                'label': 'data(label)',
-                'text-valign': 'center',
-                'text-halign': 'center'
-              }
-            },
-            {
-              selector: '.labeled',
-              style: {
-                'border-width': 3,
-                'border-color': 'green'
-              }
-            },
-            {
-              selector: '.scanned',
-              style: {
-                'background-color': 'blue'
-              }
-            },
-            {
-              selector: '.marked-edge',
-              style: {
-                'line-color': 'black',
-                'target-arrow-color': 'black',
-              }
-            }
-          ]}
-          style={ { width: window.innerWidth, height: window.innerHeight } }
-        />
+        <Button onClick={() => this.handlePrevStep()}>Prev Step</Button>
+        <Button onClick={() => this.handleNextStep()}>Next Step</Button>
+        <span>Step: <b>{this.state.stepCounter}</b> {this.state.terminated ? '(TERMINATED)' : ''}</span>
+        <span>LABELED: {JSON.stringify(this.convertSetOfNodesToLabels(this.state.nodesLabeled))}</span>
+        <span>SCANNED: {JSON.stringify(this.convertSetOfNodesToLabels(this.state.nodesScanned))}</span>
+        <div style={{ display: 'flex', width: '100%', height: 'calc(100% - 100px)'}}>
+          <CytoscapeComponent
+            elements={this.toCytoscapeNetwork()}
+            layout={layout}
+            stylesheet={stylesheet}
+            style={ { width: '100%', height: window.innerHeight } }
+          />
+          <CytoscapeComponent
+            elements={this.toCytoscapeResidual()}
+            layout={layout}
+            stylesheet={stylesheet}
+            style={ { width: '100%', height: window.innerHeight } }
+          />
+        </div>
       </div>
     );
   }
